616970861bdfe5186f907767a2020617fdfa68d6
Berserkur <viktor.snaer23@gmail.com>
2022-02-07 20:06:04 +0000

    Introduction text with gradient, background color

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,25 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
+    <div className="home-page-container">
       <Head>
         <title>Viktor Snaer</title>
         <meta
           name="description"
-          content="Viktor Snaer portfolio showcasing projects and blog"
+          content="Viktor Snaer portfolio showcasing projects"
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1>Portfolio</h1>
+      <div className="introduction">
+        <h1 className="headline-l gradient">
+          Hi,
+          <br /> I'm Viktor Snaer Web Developer
+        </h1>
+        <h3 className="headline-s gradient">Front End Developer</h3>
+        <button className="button">Contact me</button>
+      </div>
     </div>
   );
 };
