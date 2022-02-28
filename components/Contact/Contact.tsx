7e4920ba14b89645fd40e13d54aabaf648960a5e
Berserkur <viktor.snaer23@gmail.com>
2022-02-28 16:25:55 +0000

    contact me form connected to email

--- components/Contact/Contact.tsx
+++ components/Contact/Contact.tsx
@@ -1,13 +1,9 @@
 import React from "react";
 
 import classes from "./Contact.module.css";
+import Form from "./Form";
 
 const Contact = () => {
-  const handleOnSubmit = (e: React.ChangeEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    console.log("handleOnSubmit function called");
-  };
-
   return (
     <div className={`screen-size ${classes.contactContainer}`} id="contact">
       <h3 className={`section-title`}>Contact Me</h3>
@@ -17,14 +13,7 @@ const Contact = () => {
         soluta! Atque earum asperiores ab labore quibusdam in aperiam deserunt
         fugiat possimus!
       </p>
-      <div className={classes.formContainer}>
-        <form onSubmit={handleOnSubmit} className={classes.contactForm}>
-          <input type={"email"} placeholder="Email" required />
-          <input type={"text"} placeholder="Name" required />
-          <textarea placeholder="Message" required />
-          <button type="submit">Submit</button>
-        </form>
-      </div>
+      <Form />
     </div>
   );
 };
---
+++ components/Contact/Form.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+
+import classes from "./Contact.module.css";
+
+import Swal from "sweetalert2";
+import emailjs, { init } from "@emailjs/browser";
+
+const Form = () => {
+  const USER_ID = process.env.NEXT_PUBLIC_USER_ID as string;
+  const SERVICE_ID = process.env.NEXT_PUBLIC_SERVICE_ID as string;
+  const TEMPLATE_ID = process.env.NEXT_PUBLIC_TEMPLATE_ID as string;
+
+  init(USER_ID);
+
+  const handleOnSubmit = (e: React.ChangeEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("handleOnSubmit function called");
+    emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, e.target, USER_ID).then(
+      (result) => {
+        console.log(result.text);
+        Swal.fire({
+          icon: "success",
+          title: "Message Sent Successfully",
+        });
+      },
+      (error) => {
+        console.log(error.text);
+        Swal.fire({
+          icon: "error",
+          title: "Something went wrong",
+          text: error.text,
+        });
+      }
+    );
+    e.target.reset();
+  };
+
+  return (
+    <div className={classes.formContainer}>
+      <form onSubmit={handleOnSubmit} className={classes.contactForm}>
+        <input name="from_email" type={"email"} placeholder="Email" required />
+        <input name="from_name" type={"text"} placeholder="Name" required />
+        <textarea name="message" placeholder="Message" required />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default Form;
