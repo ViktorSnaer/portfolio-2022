4da5a9ff3e638d534746346bb14943b41590ac33
Berserkur <viktor.snaer23@gmail.com>
2022-03-03 17:18:54 +0000

    copyright

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -4,9 +4,12 @@ import Socials from "../Hero/Socials";
 import classes from "./Footer.module.css";
 
 const Footer = () => {
+  const date = new Date();
+
   return (
     <div className={`screen-size ${classes.footer}`}>
       <Socials gradient={false} />
+      <p>© Copyright {date.getFullYear()} Viktor Snaer</p>
       <p>Reykjavik Iceland UTC 0</p>
     </div>
   );
