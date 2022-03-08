baa2bb6ae1d64bd28e229e5a6f5c60a5513657b8
Berserkur <viktor.snaer23@gmail.com>
2022-03-08 11:57:34 +0000

    project structure

--- pages/project/index.tsx
+++ pages/project/index.tsx
@@ -2,9 +2,18 @@ import React from "react";
 
 const index = () => {
   return (
-    <div>
+    <section className="screen-size">
+      <h3>Link back to home page</h3>
       <h3>This is the projects page</h3>
-    </div>
+      <p>Description of the project</p>
+      <ul>
+        <li>type - team project</li>
+        <li>stack - react.....</li>
+        <li>repo</li>
+        <li>link to site</li>
+      </ul>
+      <h3>Large Image of site</h3>
+    </section>
   );
 };
 
