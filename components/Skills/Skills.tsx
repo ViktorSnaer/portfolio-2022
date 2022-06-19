24a4220e544f21ab6d3c3be1315bf2eee235a961
Berserkur <viktor.snaer23@gmail.com>
2022-06-19 17:00:36 +0000

    skills text icons

--- components/Skills/Skills.tsx
+++ components/Skills/Skills.tsx
@@ -1,14 +1,13 @@
 import React from "react";
 import Skill from "./Skill";
-import styles from "./Skill.module.css";
+import styles from "./Skills.module.css";
 
 export default function Skills() {
-  const skillContent = {
-    __html: (
-      <div>
+  const skillContent = [
+    {
+      __html: `
         <svg
           xmlns="http://www.w3.org/2000/svg"
-          className={styles.svg}
           fill="none"
           viewBox="0 0 24 24"
           stroke="currentColor"
@@ -22,7 +21,6 @@ export default function Skills() {
         </svg>
         <svg
           xmlns="http://www.w3.org/2000/svg"
-          className={styles.svg}
           fill="none"
           viewBox="0 0 24 24"
           stroke="currentColor"
@@ -36,7 +34,6 @@ export default function Skills() {
         </svg>
         <svg
           xmlns="http://www.w3.org/2000/svg"
-          className={styles.svg}
           fill="none"
           viewBox="0 0 24 24"
           stroke="currentColor"
@@ -47,20 +44,69 @@ export default function Skills() {
             strokeLinejoin="round"
             d="M9.75 17L9 20l-1 1h8l-1-1-.75-3M3 13h18M5 17h14a2 2 0 002-2V5a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
           />
-        </svg>
-      </div>
-    ),
-    title: "Responsive Websites",
-    subHeading: "Creating Websites that work on all different screens",
-  };
+        </svg>`,
+      title: "Responsive Websites",
+      subHeading: "Creating Websites that work on different screens",
+    },
+    {
+      __html: `
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="1">
+      <path stroke-linecap="round" stroke-linejoin="round" d="M14 10l-2 1m0 0l-2-1m2 1v2.5M20 7l-2 1m2-1l-2-1m2 1v2.5M14 4l-2-1-2 1M4 7l2-1M4 7l2 1M4 7v2.5M12 21l-2-1m2 1l2-1m-2 1v-2.5M6 18l-2-1v-2.5M18 18l2-1v-2.5" />
+    </svg>
+`,
+      title: "Website Design",
+      subHeading:
+        "Working with a design or making a new design for the project",
+    },
+    {
+      __html: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="1">
+    <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z" />
+  </svg>`,
+      title: "User Authentication",
+      subHeading:
+        "Authenticating users to gain access to the site or parts of the site for example",
+    },
+    {
+      __html: `<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="1">
+      <path stroke-linecap="round" stroke-linejoin="round" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
+    </svg><svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="1">
+    <path stroke-linecap="round" stroke-linejoin="round" d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+  </svg>`,
+      title: "Shopify Integration",
+      subHeading: "Unlocking the ability to sell on your website",
+    },
+    {
+      __html: `<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="1">
+      <path stroke-linecap="round" stroke-linejoin="round" d="M4 7v10c0 2.21 3.582 4 8 4s8-1.79 8-4V7M4 7c0 2.21 3.582 4 8 4s8-1.79 8-4M4 7c0-2.21 3.582-4 8-4s8 1.79 8 4m0 5c0 2.21-3.582 4-8 4s-8-1.79-8-4" />
+    </svg>`,
+      title: "Database Connection",
+      subHeading: "Connecting the website to a database to store information",
+    },
+    {
+      __html: `<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="1">
+      <path stroke-linecap="round" stroke-linejoin="round" d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z" />
+      <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+    </svg>`,
+      title: "And much more",
+      subHeading: "lorem ipsum",
+    },
+  ];
 
   return (
-    <div>
-      <Skill
-        svg={skillContent.__html}
-        title={skillContent.title}
-        subHeading={skillContent.subHeading}
-      />
+    <div className="screen-size">
+      <h1>What I can offer</h1>
+      <div className={`${styles.container}`}>
+        {skillContent.map((skill, index) => {
+          return (
+            <Skill
+              key={index}
+              svg={skill.__html}
+              title={skill.title}
+              subHeading={skill.subHeading}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 }
