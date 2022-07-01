88cad22df7f2ee650bb4254089e133156dd84e1e
Berserkur <viktor.snaer23@gmail.com>
2022-07-01 18:59:54 +0000

    work map

--- components/Work/Work.tsx
+++ components/Work/Work.tsx
@@ -2,34 +2,42 @@ import Image from "next/image";
 import styles from "./Work.module.css";
 
 export default function Work() {
+  const COMPANIES = [
+    {
+      id: 0,
+      src: "/images/companies/hot-sheep.png",
+      alt: "Hot Sheep tickets logo",
+      height: 100,
+    },
+    {
+      id: 0,
+      src: "/images/companies/plastplan.png",
+      alt: "Plastplan logo",
+      height: 100,
+    },
+    {
+      id: 0,
+      src: "/images/companies/grofuleiga.png",
+      alt: "Grofuleiga logo",
+      height: 25,
+    },
+  ];
+
   return (
     <div className="screen-size">
       <h1 className="section-title">Work experience</h1>
       <div className={styles.companyContainer}>
-        <div>
-          <Image
-            src="/images/companies/hot-sheep.png"
-            alt="lorem ipsum"
-            width="100"
-            height="100"
-          />
-        </div>
-        <div>
-          <Image
-            src="/images/companies/plastplan.png"
-            alt="lorem ipsum"
-            width="100"
-            height="100"
-          />
-        </div>
-        <div>
-          <Image
-            src="/images/companies/grofuleiga.png"
-            alt="lorem ipsum"
-            width="100"
-            height="25"
-          />
-        </div>
+        {COMPANIES.map((company) => {
+          return (
+            <Image
+              key={company.id}
+              src={company.src}
+              alt={company.alt}
+              width="100"
+              height={company.height}
+            />
+          );
+        })}
       </div>
     </div>
   );
